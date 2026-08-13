f935bfede347fb7ec2857362577613c305cf3a9a
hugozhutc <775801261@qq.com>
2026-08-13 15:04:15 +0800

    Create BootReceiver.kt

---
+++ app/src/main/java/com/example/netfloatmonitor/BootReceiver.kt
@@ -0,0 +1,22 @@
+package com.example.netfloatmonitor
+
+import android.content.BroadcastReceiver
+import android.content.Context
+import android.content.Intent
+import android.os.Build
+
+class BootReceiver : BroadcastReceiver() {
+    override fun onReceive(context: Context, intent: Intent) {
+        if (intent.action == Intent.ACTION_BOOT_COMPLETED ||
+            intent.action == "android.intent.action.QUICKBOOT_POWERON" ||
+            intent.action == "com.htc.intent.action.QUICKBOOT_POWERON") {
+            
+            val serviceIntent = Intent(context, FloatService::class.java)
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+                context.startForegroundService(serviceIntent)
+            } else {
+                context.startService(serviceIntent)
+            }
+        }
+    }
+}
